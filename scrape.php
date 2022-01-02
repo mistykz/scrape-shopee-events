b9d2a0d808895910357316b2dfeeae72cd504290
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2022-01-03 02:59:51 +0800

    delete cookie & csrftoken (don't need it anymore)
    the program can run without cookie & csrftoken

--- scrape.php
+++ scrape.php
@@ -9,11 +9,6 @@ $link = readline("Input link co1k (contoh : https://shopee.co.id/collections/994
 $match_id = getMatchID($link);
 //echo $match_id; //Debug
 
-//Read cookie from cookieAccount.txt
-$cookie = file_get_contents("cookieAccount.txt");
-$csrftoken = getCsrfToken($cookie);
-//echo $csrftoken; //Debug
-
 scrapeProductToJSON($cookie, $csrftoken, $match_id);
 
 jsonToCSV();
